95f10b6c16953cdedfc8890ebbc15e5272627d87
idss13 <italodasilva67@hotmail.com>
2024-09-29 12:47:40 -0300

    29/09 - correção de rotas

--- backend/src/routes/auth-route.js
+++ backend/src/routes/auth-route.js
@@ -6,5 +6,7 @@ const AuthService = require("../services/auth-service");
 router.post("/api/auth/register", AuthController.register);
 router.post("/api/auth/login", AuthController.login);
 router.post("/api/auth/refresh-token", AuthService.Auth, AuthController.refreshToken);
+router.post("/api/auth/request-password-reset", AuthController.requestPasswordReset);
+router.post("/api/auth/reset-password", AuthController.resetPassword);
 
 module.exports = router;
--- backend/src/routes/user-route.js
+++ backend/src/routes/user-route.js
@@ -7,7 +7,5 @@ router.get("/api/user", AuthService.Auth, UserController.findAll);
 router.get("/api/user/:id", AuthService.Auth, UserController.getId);
 router.put("/api/user/update/:id", AuthService.Auth, UserController.update);
 router.delete("/api/user/:id", AuthService.isAdmin, UserController.delete);
-router.post("/api/user/request-password-reset", UserController.requestPasswordReset);
-router.post("/api/user/reset-password", UserController.resetPassword);
 
 module.exports = router;
